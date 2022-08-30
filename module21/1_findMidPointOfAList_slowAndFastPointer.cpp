a99e3e09608cd352926f6903c01a7943fc6a95e1
zilani131 <zilani.cuet@gmail.com>
2022-08-30 12:24:10 +0600

    remove cycle ,make cycle and cycle status

--- module21/1_findMidPointOfAList_slowAndFastPointer.cpp
+++ module21/1_findMidPointOfAList_slowAndFastPointer.cpp
@@ -1,64 +1,156 @@
 #include<bits/stdc++.h>
 using namespace std;
-class Node{
+class Node
+{
 public:
     int value;
     Node *Next;
-    Node(int val){
+    Node(int val)
+    {
 
-    value=val;
-    Next=NULL;
+        value=val;
+        Next=NULL;
     }
 
 };
-void display(Node *head){
-Node*temp=head;
-while(temp!=NULL){
-    cout<<temp->value<<" ";
-    temp=temp->Next;
-}
+void display(Node *head)
+{
+    Node*temp=head;
+    while(temp!=NULL)
+    {
+        cout<<temp->value<<" ";
+        temp=temp->Next;
+    }
 
 }
+//////////////////////////////////////////////insert at tail///////////////
 void insertAtTail(Node* &head,int val)
 {
     Node *newNode=new Node(val);
-    if(head==NULL){
+    if(head==NULL)
+    {
         head=newNode;
         return;
     }
     Node*temp=head;
-    while(temp->Next!=NULL){
+    while(temp->Next!=NULL)
+    {
         temp=temp->Next;
     }
     temp->Next=newNode;
     return;
 
 }
-int mid(Node* head){
-Node* fast=head;
-Node* slow=head;
-if(head==NULL){
-    return -1;
-}
+//////////////////////////////////////find the mid using slow and fast pointer////////////
+int mid(Node* head)
+{
+    Node* fast=head;
+    Node* slow=head;
+    if(head==NULL)
+    {
+        return -1;
+    }
 
+    while(fast!=NULL&&fast->Next!=NULL)
+    {
+        fast=fast->Next->Next;
+        slow=slow->Next;
+    }
+    return slow->value;
+}
+//////////////////////////////////////make cycle///////////////
+void makeCycle(Node*& head,int pos){
+Node*temp=head;
+int ctn=1;
+Node*start;
+while(temp->Next!=NULL){
+    if(ctn==pos) start=temp;
+    temp=temp->Next;
+    ctn++;
+}
+temp->Next=start;
+}
+bool cycleStatus(Node *&head){
+Node*slow=head;
+Node*fast=head;
 while(fast!=NULL&&fast->Next!=NULL){
+
+    slow=slow->Next;
     fast=fast->Next->Next;
+    if(slow==fast){
+        return true;
+    }
+}
+return false;
+}
+void removeCycle(Node* &head){
+Node*fast=head;
+Node*slow=head;
+do{
+    slow=slow->Next;
+    fast=fast->Next->Next;
+}
+while(slow!=fast);
+    fast=head;
+while(slow->Next!=fast->Next){
+    fast=fast->Next;
     slow=slow->Next;
 }
-return slow->value;
+  slow->Next=NULL;
 }
-int main(){
+int main()
+{
     Node* head=NULL;
-int n;
-cin>>n;
-int k;
-while(n--){
-    cout<<"Enter the value ";
-        cin>>k;
-    insertAtTail(head,k);
-}
-cout<<endl;
-display(head);
-int val=mid(head);
-cout<<endl<<val;
+
+    cout<<"1.Insert at tail"<<endl<<"2.find the mid using slow first pointer"<<endl<<"3.make a cycle in the list "<<endl<<"4.cycle status"<<endl<<"5.Remove Cycle"<<endl;
+    int choice=100;
+               while(choice!=0)
+    {
+        cout<<"Enter the choice " ;
+        cin>>choice;
+        int val,pos,m;
+        switch(choice)
+        {
+        case 1:
+            cout<<"Enter the value ";
+            cin>>val;
+            insertAtTail(head,val);
+            break;
+        case 2:
+            m=mid(head);
+            cout<<m;
+            break;
+        case 3:
+            cout<<"Insert the position ";
+            cin>>pos;
+            makeCycle(head,pos);
+            break;
+        case 4:
+            bool t;
+            t=cycleStatus(head);
+           // cout<<t;
+            if(t){
+                cout<<"There is cycle "<<endl;
+            }
+            else{
+                cout<<"There is no cycle"<<endl;
+            }
+            break;
+        case 5:
+            if(cycleStatus(head)){
+                 removeCycle(head);
+            }
+            else{
+                cout<<"There is no cycle to remove ";
+            }
+
+            break;
+        }
+
+
+    }
+    cout<<endl;
+    display(head);
+    int val=mid(head);
+    cout<<endl<<val;
 }
