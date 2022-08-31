5d0232ace16a2dae38ceb8260abf07c40d0ddd72
zilani131 <zilani.cuet@gmail.com>
2022-08-31 20:23:28 +0600

    roteted every k node

---
+++ module22.5_practice/4.cpp
@@ -0,0 +1,98 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node {
+public:
+    int  value;
+    Node *Next;
+    Node(int  val){
+    value=val;
+    Next=NULL;
+
+    }
+
+};
+void display(Node*& head){
+Node*temp=head;
+while(temp!=NULL){
+    cout<<temp->value;
+   if(temp->Next!=NULL) cout<<"->";
+    temp=temp->Next;
+}
+
+}
+void add(Node*&head,int  val){
+    Node *newNode=new Node(val);
+if(head==NULL){
+    head=newNode;
+    return;
+}
+Node *temp=head;
+while(temp->Next!=NULL){
+    temp=temp->Next;
+}
+temp->Next=newNode;
+return;
+}
+Node *reverseList(Node*& head){
+Node*Pre=NULL;
+Node*Curr=head;
+Node*Nxt;
+while(Curr!=NULL){
+    Nxt=Curr->Next;
+    Curr->Next=Pre;
+    Pre=Curr;
+    Curr=Nxt;
+
+}
+
+return Pre;}
+Node * rotate(Node *&head,int pos){
+Node*start;
+Node*temp=head;
+Node* rot=head;
+int ctn=1;
+//cout<<"Hei";
+while(ctn<pos){
+    temp=temp->Next;
+    ctn++;
+}
+
+start =temp->Next;
+
+Node *temp2;
+temp->Next=NULL;
+Node*first=reverseList(head);
+//cout<<endl;
+//display(first);
+Node*second=reverseList(start);
+//cout<<endl;
+//display(second);
+temp2=first;
+while(temp2->Next!=NULL){
+   temp2=temp2->Next;
+
+}
+temp2->Next=second;
+
+return first;
+}
+int main(){
+
+  Node *head=NULL;
+int t;
+cin>>t;
+int a;
+while(t--){
+    cin>>a;
+    add(head,a);
+
+}
+int pos;
+cin>>pos;
+//display(head);
+
+//cout<<endl;
+Node*ro=rotate(head,pos);
+display(ro);
+}
+
